a0041da5cf8e0358e4b5d2c62bf4d5d086b19f52
Administrator <Administrator@linuxea>
2017-04-28 21:39:08 +0800

    不改动service了

--- wozi/src/main/java/com/wozi/base/BaseService.java
+++ wozi/src/main/java/com/wozi/base/BaseService.java
@@ -1,69 +1,35 @@
 package com.wozi.base;
 
-
-import java.util.Map;
-
-import com.google.common.collect.Maps;
 import com.jfinal.plugin.activerecord.Model;
-import com.jfinal.plugin.activerecord.Table;
-import com.jfinal.plugin.activerecord.TableMapping;
 
-/*
- *@date      2017年4月28日 下午12:13:21
- *@desc      基础serivce
- *@version   1.0
- *@author    linuxea
- */
-@SuppressWarnings("unchecked")
+/**curd base service*/
 public class BaseService<T extends Model<?>> {
 	
 	/**model save*/
-	protected boolean add(T model) {
-		return model.save();
+	public boolean add(T model) {
+		boolean isSuccess = false;
+		isSuccess = model.save();
+		return isSuccess;
 	}
 	
 	/**model del*/
-	protected boolean del(T model) {
-		return model.delete();
+	public boolean del(T model) {
+		boolean isSuccess = false;
+		isSuccess = model.delete();
+		return isSuccess;
 	}
 	
 	/**model update*/
-	protected boolean update(T model) {
-		return model.update();
+	public boolean update(T model) {
+		boolean isSuccess = false;
+		isSuccess = model.update();
+		return isSuccess;
 	}
-	
 	
 	/**model return newID*/
-	protected <R> R  saveAndGetKey(T model) {
-		String idColName = keyPrimaryColumnName(model);
-		if(model.save()){
-			return (R) model.get(idColName);
-		}
-		
-		return null;
-		
+	public Object getId(T model, String idName) {
+		model.save();
+		return model.get(idName);
 	}
-	
-	protected <K, V> Map<K,V> saveAndGetSomeColumnsValues(T model, String...params){
-		Map<K,V> map = Maps.newHashMap();
-		if(model.save()){
-			for(int i = 0;i<params.length;i++){
-				map.put((K)params[i],(V)model.get(params[i]));
-			}
-		}else{
-			return null;
-		}
-		
-		return map;
-		
-	}
-	
-	//通过model去找id列名 :) 一般是单主键不考虑复合主键的情况。否则返回map也不是很方便
-	private String keyPrimaryColumnName(T t){
-		Table table = TableMapping.me().getTable(t.getClass());
-		return table.getPrimaryKey()[0];
-	}
-	
 	
 }
-
