c2d011d30325dc6f4f27071a21933d07acb30f49
linuxea.lin <linuxea.lin@Zu-PC63.bamboonetworks.com>
2017-04-28 19:33:24 +0800

    完善Service

--- wozi/src/main/java/com/wozi/base/BaseService.java
+++ wozi/src/main/java/com/wozi/base/BaseService.java
@@ -1,35 +1,52 @@
 package com.wozi.base;
 
-import com.jfinal.plugin.activerecord.Model;
 
-/**curd base service*/
+import com.jfinal.plugin.activerecord.Model;
+import com.jfinal.plugin.activerecord.Table;
+import com.jfinal.plugin.activerecord.TableMapping;
+
+/*
+ *@date      2017年4月28日 下午12:13:21
+ *@desc      基础serivce
+ *@version   1.0
+ *@author    linuxea
+ */
+
 public class BaseService<T extends Model<?>> {
 	
 	/**model save*/
-	public boolean add(T model) {
-		boolean isSuccess = false;
-		isSuccess = model.save();
-		return isSuccess;
+	protected boolean add(T model) {
+		return model.save();
 	}
 	
 	/**model del*/
-	public boolean del(T model) {
-		boolean isSuccess = false;
-		isSuccess = model.delete();
-		return isSuccess;
+	protected boolean del(T model) {
+		return model.delete();
 	}
 	
 	/**model update*/
-	public boolean update(T model) {
-		boolean isSuccess = false;
-		isSuccess = model.update();
-		return isSuccess;
+	protected boolean update(T model) {
+		return model.update();
 	}
 	
+	@SuppressWarnings("unchecked")
 	/**model return newID*/
-	public Object getId(T model, String idName) {
-		model.save();
-		return model.get(idName);
+	protected <R> R  saveAndGetKey(T model) {
+		String idColName = keyPrimaryColumnName(model);
+		if(model.save()){
+			return (R) model.get(idColName);
+		}else{
+			return null;
+		}
+		
 	}
+	
+	//通过model去找id列名 :) 一般是单主键不考虑复合主键的情况。否则返回map也不是很方便
+	private String keyPrimaryColumnName(T t){
+		Table table = TableMapping.me().getTable(t.getClass());
+		return table.getPrimaryKey()[0];
+	}
+	
 	
 }
+
